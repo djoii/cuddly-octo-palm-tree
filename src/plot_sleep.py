ca2767d1c65d38307eb4a75e906a688399cfe982
Durand O'Meara II <djoii.omeara@gmail.com>
2019-04-22 15:08:32 -0400

    merge with iPad version

--- src/plot_sleep.py
+++ src/plot_sleep.py
@@ -2,42 +2,62 @@ import csv
 import itertools
 import matplotlib.pyplot as plt
 
-with open('../csvs/AutoSleep.csv') as f:
+
+def csv2list(f):
     reader = csv.reader(f)
     data = list(reader)
     data.pop(0)
     samples = len(data)
+    return samples, data
+
+
+with open('AutoSleep.csv') as f:
+    samples, data = csv2list(f)
+
+
+def declutter(r, l):
+    short_row = list(itertools.islice(r, 2))
+    split_row = [x.split() for x in short_row]
+    joind_row = [split_row[0][1], split_row[1][1]]
+    l.append(joind_row)
+
 
 sleeps = []
 for row in data:
-    new_row = list(itertools.islice(row, 2))
-    new_row2 = [x.split() for x in new_row]
-    # print(new_row2)
-    sleep = [new_row2[0][1], new_row2[1][1]]
-    sleeps.append(sleep)
+    declutter(row, sleeps)
+
+
+def psudotime(t, l):
+    hour, minute, second = t.split(':')
+    fraction_of_day = (
+        int(hour) * 3600 + int(minute) * 60 + int(second)) / 86400 * 48
+    l.append(int(fraction_of_day))
+
 
 fractional_sleeps = []
 for sleep in sleeps:
     fractions_of_day = []
     for time in sleep:
-        hour, minute, second = time.split(':')
-        fraction_of_day = (int(hour)*3600 + int(minute)
-                           * 60 + int(second))/86400*48
-        fractions_of_day.append(int(fraction_of_day))
+        fraction_of_day = psudotime(time, fractions_of_day)
     fractional_sleeps.append(fractions_of_day)
+
+
+def binning(dt, l):
+    start = min(dt)
+    end = max(dt)
+    bins = list(range(start, end + 1))
+    l.append(bins)
+
 
 bins_list = []
 for fractional_sleep in fractional_sleeps:
-    begin = min(fractional_sleep)
-    end = max(fractional_sleep)
-    bins = list(range(begin, end+1))
-    bins_list.append(bins)
+    binning(fractional_sleep, bins_list)
 
-counts = [0]*48
+counts = [0] * 48
 for bins in bins_list:
     for bin in bins:
         counts[bin] += 1
 
-norm_counts = list(map(lambda x: x/samples, counts))
+norm_counts = list(map(lambda x: x / samples, counts))
 plt.bar(list(range(0, 48)), norm_counts)
 plt.show()
